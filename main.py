d9ccc0ee5137bdeb16145a43c4b7bceccfe8f344
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-02-02 19:14:01 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,218 @@
+#a program that lets the user input a letter and display if the letter is vowel or consonant
+while True:
+    letter = input("Enter a letter: ")
+    #consonant
+    if letter == "a":
+        print("vowel")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "e":
+        print("vowel")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "i":
+        print("vowel")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "o":
+        print("vowel")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "u":
+        print("vowel")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    #consonant
+    elif letter == "b":
+        print("consonant")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "c":
+        print("consonant")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "d":
+        print("consonant")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "f":
+        print("consonant")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "g":
+        print("consonant")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "h":
+        print("consonant")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "j":
+        print("consonant")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "k":
+        print("consonant")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "l":
+        print("consonant")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "m":
+        print("consonant")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "n":
+        print("consonant")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "p":
+        print("consonant")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "q":
+        print("consonant")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "r":
+        print("consonant")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "s":
+        print("consonant")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "t":
+        print("consonant")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "v":
+        print("consonant")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "w":
+        print("consonant")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "x":
+        print("consonant")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "y":
+        print("consonant")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    elif letter == "z":
+        print("consonant")
+        repeatChoice = input("do you want to use it again (y/n): ")
+        if repeatChoice == "y":
+            print()
+        elif repeatChoice == "n":
+            print("thank you!")
+            break
+    else:
+        print("Invalid, please input a letter")
+        print()
+
+
